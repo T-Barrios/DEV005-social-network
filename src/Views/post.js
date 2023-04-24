81058fd98ff23b998a30f7e8bf4b8baca1903b49
NatSama2 <vet.nat.c@gmail.com>
2023-04-24 19:13:00 -0400

    se publican post, se inicia con funcion like

--- src/Views/post.js
+++ src/Views/post.js
@@ -1,11 +1,11 @@
-import { addDoc, collection, doc } from 'firebase/firestore';
+import {
+  addDoc, collection, doc, getDocs, updateDoc, onSnapshot, query,
+} from 'firebase/firestore';
 import { async } from 'regenerator-runtime';
 import { auth, db } from '../lib';
 import { signOut } from '../lib/firebase-service';
 
 function post() {
-  const inputPost = '';
-
   const section = document.createElement('article');
   section.className = 'containerSection';
   // Contenedor header post
@@ -22,39 +22,7 @@ function post() {
   userIcon.src = './user-icon/icon-green-mushroom.png';
   userIcon.id = 'userIcon';
   userIcon.className = 'modalOpen';
-  // Container main
-  const containerMain = document.createElement('section');
-  containerMain.className = 'containerMain';
-  // Container header post
-  const containerHeaderPost = document.createElement('div');
-  containerHeaderPost.className = 'containerHeaderPost';
-  const containerUserEmail = document.createElement('div');
-  containerUserEmail.className = 'containerUserEmail';
-  const userEmail = document.createElement('p');
-  userEmail.textContent = 'Mario';
-  const containerUserIconPost = document.createElement('div');
-  containerUserIconPost.className = 'containerUserIconPost';
-  const userIconPost = document.createElement('img');
-  userIconPost.src = './user-icon/icon-red-mushroom.png';
-  userIconPost.id = 'userIconPost';
-  // Container post
-  const containerPost = document.createElement('div');
-  containerPost.className = 'containerPost';
-  const txtPost = document.createElement('p');
-  txtPost.textContent = inputPost;
-  // Container footer post
-  const containerFooterPost = document.createElement('div');
-  containerFooterPost.className = 'containerFooterPost';
-  const containerNumberOfLikes = document.createElement('div');
-  containerNumberOfLikes.className = 'containerNumberOfLikes';
-  const numberOfLikes = document.createElement('p');
-  numberOfLikes.textContent = '2';
-  numberOfLikes.id = 'numberOfLikes';
-  const containerLikesIcon = document.createElement('div');
-  containerLikesIcon.className = 'containerLikesIcon';
-  const likesIcon = document.createElement('img');
-  likesIcon.src = './Img/icon-likes.png';
-  likesIcon.id = 'likesIcon';
+
   // Container menu
   const containerMenu = document.createElement('div');
   containerMenu.className = 'containerMenu';
@@ -89,11 +57,13 @@ function post() {
   divModalPost.className = 'modalContentPost';
   const textPost = document.createElement('textarea');
   textPost.className = 'textPost';
-  textPost.placeholder = 'que wa queri';
+  textPost.placeholder = 'Hola, que hace?';
+  textPost.required = 'true';
   const btnPublish = document.createElement('button');
   btnPublish.textContent = 'Publicar';
   btnPublish.id = 'btnPublish';
   btnPublish.className = 'button';
+  btnPublish.type = 'submit';
 
   divModalPost.append(textPost, btnPublish);
   containerModalPost.append(divModalPost);
@@ -101,20 +71,12 @@ function post() {
   divModal.append(spanCloseModal, btnLogOut, btnMyWall);
   containerModal.append(divModal);
 
-  containerLogoPost.append(logoPost);
-  containerUserIconPost.append(userIconPost);
-  containerUserEmail.append(userEmail);
   containerUserIcon.append(userIcon, containerModal);
-  containerFooterPost.append(containerNumberOfLikes, containerLikesIcon);
-  containerNumberOfLikes.append(numberOfLikes);
-  containerLikesIcon.append(likesIcon);
-  containerHeaderPost.append(containerUserIconPost, containerUserEmail);
-  containerPost.append(txtPost);
+  containerLogoPost.append(logoPost);
 
   containerHeader.append(containerLogoPost, containerUserIcon);
-  containerMain.append(containerHeaderPost, containerPost, containerFooterPost);
   containerMenu.append(btnNewPost);
-  section.append(containerHeader, containerMain, containerMenu, containerModalPost);
+  section.append(containerHeader, containerMenu, containerModalPost);
 
   btnLogOut.addEventListener('click', async () => {
     await signOut(auth);
@@ -147,15 +109,110 @@ function post() {
     }
   };
 
+  // Agregar post
   btnPublish.addEventListener('click', async () => {
     const docRef = await addDoc(collection(db, 'post'), {
-      /* title: '', */
+      author: auth.currentUser.email,
       content: textPost.value,
+      like: 0,
     });
-    console.log('Document written with ID: ', docRef.id);
-    console.log('texto', textPost.value);
-    txtPost.textContent = textPost.value;
+    // console.log('Document written with ID: ', docRef.id);
+    // console.log('texto', textPost.value);
+    containerModalPost.style.display = 'none';
   });
+
+  // Obtener todos los post
+  async function getAllPosts() {
+    const querySnapshot = await getDocs(collection(db, 'post'));
+    querySnapshot.forEach((docs) => {
+      // doc.data() is never undefined for query doc snapshots
+      const docData = docs.data();
+      // console.log(docs.id, ' => ', docData.author);
+      // Container main
+      const containerMain = document.createElement('section');
+      containerMain.className = 'containerMain';
+      // Container header post
+      const containerHeaderPost = document.createElement('div');
+      containerHeaderPost.className = 'containerHeaderPost';
+      const containerUserEmail = document.createElement('div');
+      containerUserEmail.className = 'containerUserEmail';
+      const userEmail = document.createElement('p');
+      userEmail.textContent = docData.author;
+      const containerUserIconPost = document.createElement('div');
+      containerUserIconPost.className = 'containerUserIconPost';
+      const userIconPost = document.createElement('img');
+      userIconPost.src = './user-icon/icon-red-mushroom.png';
+      userIconPost.id = 'userIconPost';
+      // Container post
+      const containerPost = document.createElement('div');
+      containerPost.className = 'containerPost';
+      const txtPost = document.createElement('p');
+      txtPost.textContent = docData.content;
+      // Container footer post
+      const containerFooterPost = document.createElement('div');
+      containerFooterPost.className = 'containerFooterPost';
+      const containerNumberOfLikes = document.createElement('div');
+      containerNumberOfLikes.className = 'containerNumberOfLikes';
+      const numberOfLikes = document.createElement('p');
+      numberOfLikes.textContent = docData.like;
+      numberOfLikes.id = 'numberOfLikes';
+      const containerLikesIcon = document.createElement('div');
+      containerLikesIcon.className = 'containerLikesIcon';
+      const likesIcon = document.createElement('img');
+      likesIcon.src = './Img/icon-likes.png';
+      likesIcon.id = 'likesIcon';
+
+      // apendizaje de container main (post)
+      containerUserIconPost.append(userIconPost);
+      containerUserEmail.append(userEmail);
+      containerFooterPost.append(containerNumberOfLikes, containerLikesIcon);
+      containerNumberOfLikes.append(numberOfLikes);
+      containerLikesIcon.append(likesIcon);
+      containerHeaderPost.append(containerUserIconPost, containerUserEmail);
+      containerPost.append(txtPost);
+      containerMain.append(containerHeaderPost, containerPost, containerFooterPost);
+      section.append(containerMain);
+
+      // Dar like
+      const likeRef = doc(db, 'post', 'like');
+      likesIcon.addEventListener('click', async () => {
+        await updateDoc(likeRef, {
+          like: true,
+        });
+      });
+    });
+  }
+  getAllPosts();
+  /*
+  async function queryListener() {
+    const queryPost = query(
+      collection(db, 'post'),
+    );
+    onSnapshot(queryPost, (querySnapshot) => {
+      console.log('query', querySnapshot);
+    });
+  }
+
+  queryListener(); */
+  // Agregar listener para nuevos posts
+  const queryPost = query(collection(db, 'post'));
+  onSnapshot(queryPost, (querySnapshot) => {
+    querySnapshot.forEach((snap) => {
+      console.log('snap data: ', snap.data());
+    });
+  });
+
+  /*
+  const queryPost = query(collection(db, 'post'));
+  onSnapshot(queryPost, (querySnapshot) => {
+    querySnapshot.docChanges().forEach((docs) => {
+       if (docs.type === 'added') {
+        const docData = docs.doc.data();
+        console.log(docs.doc.id, ' => ', docData.author);
+        getAllPosts(docs.doc);
+      }
+    });
+  }); */
 
   document.body.style.backgroundColor = '#262523';
   return section;
